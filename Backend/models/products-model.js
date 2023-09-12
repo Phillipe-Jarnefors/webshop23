262e47c13dca83d44ec18d87956c3b57137be6ca
Phillipe <phillipe_jarnefors@hotmail.com>
2023-09-12 13:53:55 +0200

    Fix small issues

--- Backend/models/products-model.js
+++ Backend/models/products-model.js
@@ -1,23 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const ProductSchema = mongoose.Schema({
   name: { type: String, required: true },
-<<<<<<< HEAD
   image: { type: String, required: true },
-=======
-  image: { type: String },
->>>>>>> FEsetupData
   shortDesc: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
   isAvailable: { type: Boolean, required: true },
-<<<<<<< HEAD
-});
-
-module.exports = mongoose.model("product", ProductSchema);
-=======
 })
 
 module.exports = mongoose.model("product", ProductSchema)
->>>>>>> FEsetupData
