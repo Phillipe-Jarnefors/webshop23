562d62b33d472d6eee3398bf91cde6c703e913c0
unknown <flod96@hotmail.com>
2023-09-12 15:36:11 +0200

    Admin Functions

---
+++ Frontend/src/Pages/Admin/AdminProducts.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AdminProducts() {
+  return <div>AdminProducts</div>;
+}
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,33 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements  } from "react-router-dom"
-import './index.css'
-import Layout from "./components/Layout"
-import AllProducts, { loader as allProductsLoader } from './pages/Products/AllProducts'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import "./index.css";
+import Layout from "./components/Layout";
+import AllProducts, {
+  loader as allProductsLoader,
+} from "./pages/Products/AllProducts";
+import AdminParent, {
+  loader as adminProductsLoader,
+} from "./Pages/Admin/AdminParent";
 
-
-
-const router = createBrowserRouter(createRoutesFromElements(
-  <>
-    <Route path='/' element={<Layout />}>
-      <Route 
-        index 
-        element={<AllProducts />} 
-        loader={allProductsLoader}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<AllProducts />} loader={allProductsLoader} />
+      </Route>
+      <Route
+        path="/admin"
+        element={<AdminParent />}
+        loader={adminProductsLoader}
       />
-    </Route>
-  </>
-))
-
-
-function App () {
-  return (
-    <RouterProvider router={router} />
+    </>
   )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
